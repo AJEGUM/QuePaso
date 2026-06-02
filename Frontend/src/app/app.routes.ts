990f3a4a86f3485090de47d56b50cd152361028b
AJEGUM <juliandguevara12@gmail.com>
2026-06-01 22:44:49 -0500

    Frontend V1

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'inicio',
+    loadComponent: () => import('./pages/inicio/inicio').then(m => m.Inicio)
+  },
+  {
+    path: '',
+    redirectTo: 'inicio',
+    pathMatch: 'full'
+  },
+  {
+    path: '**',
+    redirectTo: 'inicio'
+  }
+];
